9989b8d797f01ea1f7e574fa4fd8a02e466d81cd
Briana <brianapowers78@gmail.com>
2026-04-01 19:43:18 -0500

    Removed duplicate cart icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,14 +4,17 @@ function Header() {
   return (
     <header className="header-wrapper">
       <div className="header-inner">
+        
         <h2 className="site-title">
-  <span className="cart-icon">🛒</span> Grocery Budget Tracker
-</h2>
+          <span className="cart-icon">🛒</span>
+          Grocery Budget Tracker
+        </h2>
 
         <nav className="nav-links">
           <Link to="/">Home</Link>
           <Link to="/about">About</Link>
         </nav>
+
       </div>
     </header>
   );
